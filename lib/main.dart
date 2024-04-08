a3da099bd4543a31fd1d5124b7b927f073539a0c
Sam Luther <samluther998@gmail.com>
2024-04-08 09:49:28 -0600

    Monday Updates Apr 8

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,84 @@ void main() {
   runApp(TheApp());
 }
 
+class TextBox extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TextBoxState();
+}
+
+class _TextBoxState extends State<TextBox> {
+  final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _printLatestValue() {
+    final text = myController.text;
+    print("second $text (${text.characters.length})");
+    var res = http.get(Uri.parse(
+        'http://localhost:5000/cs-322-0/us-central1/helloWorld?number=${text}'));
+    res.then((value) => print(value.body));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          onChanged: (value) {
+            print("first $value (${value.characters.length})");
+          },
+        ),
+        TextField(
+          controller: myController,
+        ),
+      ],
+    );
+  }
+}
+
+class changingBox extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _changingBoxState();
+}
+
+class _changingBoxState extends State<changingBox> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+              color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+            ),
+            child: Center(
+                child: Text(
+              _active ? "Active" : "Inactive",
+              style: const TextStyle(fontSize: 32, color: Colors.white),
+            ))));
+  }
+}
+
 class TheApp extends StatelessWidget {
   void getReq(int entry) {
     var res = http.get(Uri.parse(
@@ -29,11 +107,13 @@ class TheApp extends StatelessWidget {
             Container(
               height: 200,
               color: Colors.blue,
+              child: TextBox(),
             ),
             Container(
-              height: 100,
-              width: 100,
+              height: 200,
+              width: 200,
               color: Colors.lightBlue,
+              child: changingBox(),
             ),
             GestureDetector(
               onTap: () => getReq(entrynum++),
